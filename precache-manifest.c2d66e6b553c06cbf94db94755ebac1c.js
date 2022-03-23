0d2db28118a056f43b9dea2af932c17281d8da84
varunsingh2829 <varunsingh29@users.noreply.github.com>
2022-03-23 19:36:35 +0530

    Updates

---
+++ precache-manifest.c2d66e6b553c06cbf94db94755ebac1c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca4b54739a13bb16727216275586e5f",
+    "url": "/apple_store/index.html"
+  },
+  {
+    "revision": "501efaa807aeef8d8d08",
+    "url": "/apple_store/static/css/main.fe633105.chunk.css"
+  },
+  {
+    "revision": "e4afc378f396817eee64",
+    "url": "/apple_store/static/js/2.1d2220e0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/apple_store/static/js/2.1d2220e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501efaa807aeef8d8d08",
+    "url": "/apple_store/static/js/main.4c1c1303.chunk.js"
+  },
+  {
+    "revision": "bac1d879099eb8a3eca1",
+    "url": "/apple_store/static/js/runtime-main.3df33720.js"
+  },
+  {
+    "revision": "a7168afe9b1e0a362a8584d6a84b741d",
+    "url": "/apple_store/static/media/applei13.a7168afe.jpg"
+  },
+  {
+    "revision": "b3c34ed7aff7b4c8c81ad185697b74bb",
+    "url": "/apple_store/static/media/applew2.b3c34ed7.jpg"
+  },
+  {
+    "revision": "1649b0ed04eef564da0657af246060a1",
+    "url": "/apple_store/static/media/ears_pods.1649b0ed.jpg"
+  },
+  {
+    "revision": "19817b5e520bbac1ee690646caceece2",
+    "url": "/apple_store/static/media/glasses.19817b5e.jpg"
+  },
+  {
+    "revision": "f3c07af80688aa6fcbe240c951dcbef5",
+    "url": "/apple_store/static/media/logos.f3c07af8.jpg"
+  },
+  {
+    "revision": "d8addcace2559dc8f985fb310ea66c90",
+    "url": "/apple_store/static/media/macbook.d8addcac.jpg"
+  },
+  {
+    "revision": "005562763b29d48fc4c2f694613df642",
+    "url": "/apple_store/static/media/monitor.00556276.jpg"
+  },
+  {
+    "revision": "00f7d98199bcabf3753901e24a6b9e97",
+    "url": "/apple_store/static/media/speaker.00f7d981.jpg"
+  },
+  {
+    "revision": "b90efef140909635da151657e84c8995",
+    "url": "/apple_store/static/media/tv_set.b90efef1.webp"
+  }
+]);
